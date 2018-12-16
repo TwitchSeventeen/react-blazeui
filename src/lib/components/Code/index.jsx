f7e3e2292015ea5de2a1d3e0808d9ba51c7b6c2a
Luke Owen <luke@lkowen.co.uk>
2018-12-16 21:16:10 +0000

    Added Code component

---
+++ src/lib/components/Code/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+function Code (props) {
+  const { children, multiline } = props;
+  const classes = classNames('u-code', { 'u-code--multiline': multiline });
+  return (
+      <code className={classes}>
+        { children }
+      </code>
+  );
+}
+
+Code.propTypes = {
+  /**
+   * This element can have children
+   */
+  children: PropTypes.node,
+  /**
+   * Defines a multi-line code snippet
+   */
+  multiline: PropTypes.bool,
+};
+
+Code.defaultProps = {
+  children: null,
+  multiline: false,
+};
+
+export default Code;
---
+++ src/lib/components/Code/index.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import Code from './index';
+
+describe('Code', () => {
+  it('should be defined', () => {
+    expect(Code).toBeDefined();
+  });
+
+  it('renders correctly', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(
+        <Code>
+          lorem ipsum dolor
+        </Code>
+    );
+
+    const tree = renderer.getRenderOutput();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly a multi-line code snippet', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(
+        <Code multiline>
+          lorem ipsum dolor
+          sit amet
+        </Code>
+    );
+
+    const tree = renderer.getRenderOutput();
+    expect(tree).toMatchSnapshot();
+  });
+});
