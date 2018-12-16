ca785b450dd21e41208c2d2b82ed33eb89b88d43
Luke Owen <luke@lkowen.co.uk>
2018-12-16 20:17:32 +0000

    Added Key component

---
+++ src/lib/components/Key/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Key (props) {
+  const { children } = props;
+  return (
+      <kbd className="u-kbd">
+        { children }
+      </kbd>
+  );
+}
+
+Key.propTypes = {
+  /**
+   * This element can have children
+   */
+  children: PropTypes.node,
+};
+
+Key.defaultProps = {
+  children: null,
+};
+
+export default Key;
---
+++ src/lib/components/Key/index.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import Key from './index';
+
+describe('Key', () => {
+  it('should be defined', () => {
+    expect(Key).toBeDefined();
+  });
+
+  it('renders correctly', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(
+        <Key>
+          key
+        </Key>
+    );
+
+    const tree = renderer.getRenderOutput();
+    expect(tree).toMatchSnapshot();
+  });
+});
