731fb81ac309be9e21796cd7a5b15fe5dd1e7cb4
Luke Owen <luke@lkowen.co.uk>
2019-01-01 00:14:54 +0000

    Added exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,22 +5,46 @@ import Abbr from './components/Abbr';
 import Accordion from './components/Accordion';
 import Alert from './components/Alert';
 import Address from './components/Address';
+import Badge from './components/Badge';
+import Blockquote from './components/Blockquote';
 import Breadcrumbs from './components/Breadcrumbs';
+import Button from './components/Button';
+import Card from './components/Card';
+import Code from './components/Code';
 import Container from './components/Container';
+import Divider from './components/Divider';
 import Grid from './components/Grid';
 import Highlight from './components/Highlight';
+import Icon from './components/Icon';
+import Input from './components/Input';
+import Key from './components/Key';
+import List from './components/List';
 import Loud from './components/Loud';
+import Mono from './components/Mono';
+import Panel from './components/Panel';
 import Quiet from './components/Quiet';
 
 export {
   Abbr,
   Accordion,
-  Alert,
   Address,
-  Container,
-  Grid,
+  Alert,
+  Badge,
+  Blockquote,
   Breadcrumbs,
+  Button,
+  Card,
+  Code,
+  Container,
+  Divider,
+  Grid,
   Highlight,
+  Icon,
+  Input,
+  Key,
+  List,
   Loud,
+  Mono,
+  Panel,
   Quiet,
 };
