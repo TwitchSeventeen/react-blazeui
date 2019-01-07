4ead388ac2ae7a62364c112369a564d48ff2e29c
Luke Owen <luke@lkowen.co.uk>
2019-01-07 22:23:31 +0000

    Abbr spreads props

--- src/lib/components/Abbr/index.jsx
+++ src/lib/components/Abbr/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import omitOwnProps from '../../utils/omitOwnProps';
 
 function Abbr(props) {
   const { children, title, className } = props;
   const classes = classNames('u-text--help', className);
+  const spreadProps = omitOwnProps(props);
   return (
     <abbr
-      {...props}
+      {...spreadProps}
       className={classes}
       title={title}
     >
