b889374753873be721ee0dbdd3e5353f92646015
Luke Owen <luke.owen@bookinglive.com>
2020-04-25 22:30:55 +0100

    Updated CDN links

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,60 @@
 export const imports = {
+  'src/docs/custombuild.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-custombuild" */ 'src/docs/custombuild.mdx'),
+  'src/docs/readme.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-readme" */ 'src/docs/readme.mdx'),
+  'src/lib/components/Abbr/Abbr.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-abbr-abbr" */ 'src/lib/components/Abbr/Abbr.mdx'),
+  'src/lib/components/Accordion/Accordion.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-accordion-accordion" */ 'src/lib/components/Accordion/Accordion.mdx'),
+  'src/lib/components/Address/Address.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-address-address" */ 'src/lib/components/Address/Address.mdx'),
   'src/lib/components/Alert/Alert.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-alert-alert" */ 'src/lib/components/Alert/Alert.mdx'),
+  'src/lib/components/Avatar/Avatar.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-avatar-avatar" */ 'src/lib/components/Avatar/Avatar.mdx'),
+  'src/lib/components/Badge/Badge.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-badge-badge" */ 'src/lib/components/Badge/Badge.mdx'),
+  'src/lib/components/Blockquote/Blockquote.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-blockquote-blockquote" */ 'src/lib/components/Blockquote/Blockquote.mdx'),
+  'src/lib/components/Breadcrumbs/Breadcrumbs.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-breadcrumbs-breadcrumbs" */ 'src/lib/components/Breadcrumbs/Breadcrumbs.mdx'),
+  'src/lib/components/Button/Button.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-button-button" */ 'src/lib/components/Button/Button.mdx'),
+  'src/lib/components/Card/Card.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-card-card" */ 'src/lib/components/Card/Card.mdx'),
+  'src/lib/components/Code/Code.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-code-code" */ 'src/lib/components/Code/Code.mdx'),
+  'src/lib/components/Container/Container.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-container-container" */ 'src/lib/components/Container/Container.mdx'),
+  'src/lib/components/Counter/Counter.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-counter-counter" */ 'src/lib/components/Counter/Counter.mdx'),
+  'src/lib/components/Divider/Divider.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-divider-divider" */ 'src/lib/components/Divider/Divider.mdx'),
+  'src/lib/components/Grid/Grid.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-grid-grid" */ 'src/lib/components/Grid/Grid.mdx'),
+  'src/lib/components/Highlight/Highlight.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-highlight-highlight" */ 'src/lib/components/Highlight/Highlight.mdx'),
+  'src/lib/components/Icon/Icon.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-icon-icon" */ 'src/lib/components/Icon/Icon.mdx'),
+  'src/lib/components/Image/Image.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-image-image" */ 'src/lib/components/Image/Image.mdx'),
+  'src/lib/components/Input/Input.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-input-input" */ 'src/lib/components/Input/Input.mdx'),
+  'src/lib/components/Key/Key.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-key-key" */ 'src/lib/components/Key/Key.mdx'),
+  'src/lib/components/List/List.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-list-list" */ 'src/lib/components/List/List.mdx'),
+  'src/lib/components/Loud/Loud.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-loud-loud" */ 'src/lib/components/Loud/Loud.mdx'),
+  'src/lib/components/Mono/Mono.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-mono-mono" */ 'src/lib/components/Mono/Mono.mdx'),
+  'src/lib/components/Panel/Panel.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-panel-panel" */ 'src/lib/components/Panel/Panel.mdx'),
+  'src/lib/components/Quiet/Quiet.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-quiet-quiet" */ 'src/lib/components/Quiet/Quiet.mdx'),
+  'src/lib/components/Timeline/Timeline.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-timeline-timeline" */ 'src/lib/components/Timeline/Timeline.mdx'),
+  'src/lib/components/Table/Table.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-lib-components-table-table" */ 'src/lib/components/Table/Table.mdx'),
 }
--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { setConfig, hot } from 'react-hot-loader'
+import { hot } from 'react-hot-loader'
 import Theme from 'docz-theme-default'
 
-const Root = () => (
-  <Theme hashRouter={undefined} websocketUrl="ws://127.0.0.1:60505" />
-)
+const Root = () => <Theme />
 
 export default hot(module)(Root)
